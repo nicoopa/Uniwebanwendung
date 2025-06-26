737b22506af16413b02dd8fe0609f4aa50bef6f5
Nico Packhäuser <nico.packhaeuser@iu-study.org>
2025-06-26 16:47:35 +0200

    update namen

--- features.js
+++ features.js
@@ -1,8 +1,7 @@
-//kommt von nico sammito, der fickt seine sekreterin
 
 const neueAusgabe = (name, datum, kosten, kategorie) => {
 
-    const neueSekreterin = {
+    const neuesAusgabenJSONobejekt = {
         name,
         datum,
         kosten,
@@ -10,25 +9,25 @@ const neueAusgabe = (name, datum, kosten, kategorie) => {
     }
 
     const aktuelleAusgaben = window.localStorage.getItem('ausgaben') ? Array.from(JSON.parse(window.localStorage.getItem('ausgaben'))) : []
-    aktuelleAusgaben.push(neueSekreterin)
+    aktuelleAusgaben.push(neuesAusgabenJSONobejekt)
     window.localStorage.setItem("ausgaben", JSON.stringify(aktuelleAusgaben))
 
 }
 
 const ausgabenMonat = () => {
 
-    const aktuelleSekreterin = window.localStorage.getItem('ausgaben') ? Array.from(JSON.parse(window.localStorage.getItem('ausgaben'))) : []
+    const alleAusgaben = window.localStorage.getItem('ausgaben') ? Array.from(JSON.parse(window.localStorage.getItem('ausgaben'))) : []
     const date = new Date().getTime()
     const monat = 60 * 60 * 24 * 30
 
 
-    const neueSekreterin = aktuelleSekreterin.filter(value => {
+    const neuesAusgabenJSONobejekt = alleAusgaben.filter(value => {
 
         const valueDate = new Date(value.datum).getTime()
         return (date - monat) <= valueDate && valueDate <= date
 
     })
-    return neueSekreterin.reduce((accumulator, currentValue) => {
+    return neuesAusgabenJSONobejekt.reduce((accumulator, currentValue) => {
         return accumulator + currentValue.kosten
     }, 0)
 
@@ -37,15 +36,14 @@ const ausgabenMonat = () => {
 
 const letzten5Ausgaben = () => {
 
-    const aktuelleSekreterin = window.localStorage.getItem('ausgaben') ? Array.from(JSON.parse(window.localStorage.getItem('ausgaben'))) : []
-    const sort = aktuelleSekreterin.sort((a, b) => {
+    const alleAusgaben = window.localStorage.getItem('ausgaben') ? Array.from(JSON.parse(window.localStorage.getItem('ausgaben'))) : []
+    const sort = alleAusgaben.sort((a, b) => {
         return a > b
     })
 
     return sort.slice(0, 5)
 
 }
-
 
 document.querySelector("#AusgabenImMonat").innerHTML = `${ausgabenMonat()} €`
 
